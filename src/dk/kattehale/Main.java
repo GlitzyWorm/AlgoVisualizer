6e69288c434b96f5caace14979c3fb05dc6f485d
Mathias Jelshøj <mathias@kattehale.dk>
2021-07-15 14:28:58 +0200

    Initial Commit

---
+++ src/dk/kattehale/Main.java
@@ -0,0 +1,14 @@
+package dk.kattehale;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("AlgoVisualizer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
